188b193414ea0b824de86b26042e5adb92255f95
pengjiyuan <pengjiyuan@bytedance.com>
2021-07-09 16:02:07 +0800

    fix: skip empty string when to single line

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
 export function toSingleLine(str: string): string {
+  if (!str) {
+    return '';
+  }
   return str.trim().replace(/[\r\n\t]+/g, '').replace(/[\x20]{2,}/g, '').replace(/\|/g, '\\|');
 }
