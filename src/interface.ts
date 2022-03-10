e62ce96ebb5214c6e183cd4c535d3bc06b273aa3
zuozhiheng <zuozhiheng@bytedance.com>
2022-03-10 19:40:23 +0800

    feature: allow disable parse documentation comment by config

--- src/interface.ts
+++ src/interface.ts
@@ -15,6 +15,10 @@ export type defaultTypeMapT = Record<string, { type: string, tags?: TagType[] }>
 export type GenerateConfig = {
   defaultTypeMap?: defaultTypeMapT;
   sourceFilesPaths: string | string[];
+  /**
+   * Whether to skip parsing documentation comment as property description
+   */
+  strictComment?: boolean;
 }
 
 export type GenerateMarkdownConfig = GenerateConfig & {
